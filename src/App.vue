e499a766f57c7e8534c9f0693660b2b25ce95ed1
Владислав <rolit0852@gmail.com>
2025-01-16 17:49:42 +0300

    1com

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Project: {{  }}</h1>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+</script>
+
+<style scoped>
+body {
+  background-color: #e1e1e1;
+}
+
+.container {
+  display: flex;
+  gap: 12px;
+}
+
+.header {
+  display: flex;
+  gap: 12px;
+}
+</style>
